a706eadd5a848208fc61f46eea7b5e3586f21d62
Devapritam Sharma <dsarma1980@gmail.com>
2021-05-12 22:14:49 -1200

    Add files via upload

--- script.js
+++ script.js
@@ -38,3 +38,70 @@ function updateBlockImg(get_img) {
         canvas.add(block_obj);
     });
 }
+
+window.addEventListener('keydown', my_keydown);
+function my_keydown(e) {
+    keyPressed = e.keyCode;
+    if(e.shiftKey == true && keyPressed == '80'){
+        block_height = block_height + 10;
+        block_width = block_width + 10;
+        document.getElementById("currentWidth").innerHTML = block_width;
+        document.getElementById("currentHeight").innerHTML = block_height;
+    }
+    if(e.shiftKey == true && keyPressed == '77'){
+        block_width = block_width - 10;
+        block_height = block_height - 10;
+        document.getElementById("currentWidth").innerHTML = block_width;
+        document.getElementById("currentHeight").innerHTML = block_height;
+    }
+    if(keyPressed == '38'){
+        up();
+        console.log("up");
+    }
+    if(keyPressed == '40'){
+        down();
+        console.log("down");
+    }
+    if(keyPressed == '37'){
+        left();
+        console.log("left");
+    }
+    if(keyPressed == '39'){
+        right();
+        console.log("right");
+    }
+    if(keyPressed == '67'){
+        updateBlockImg('captain_america_left_hand.png');
+        console.log('c + w keys pressed');
+    }
+    if(keyPressed == '72'){
+        updateBlockImg('hulk_left_hand.png');
+        updateBlockImg('hulk_right_hand.png');
+        updateBlockImg('hulk_face.png');
+        updateBlockImg('hulk_legs.png');
+        updateBlockImg('hulkd_body.png');
+        console.log('c + w keys pressed');
+    }
+    if(keyPressed == '83'){
+        updateBlockImg('spiderman_left_hand.png');
+        updateBlockImg('spiderman_right_hand.png');
+        updateBlockImg('spiderman_face.png');
+        updateBlockImg('spiderman_legs.png');
+        updateBlockImg('spiderman_body.png');
+        console.log('c + w keys pressed');
+    }
+    if(keyPressed == '73'){
+        updateBlockImg('ironman_left_hand.png');
+        updateBlockImg('ironman_right_hand.png');
+        updateBlockImg('ironman_face.png');
+        updateBlockImg('ironman_legs.png');
+        updateBlockImg('ironman_body.png');
+        console.log('c + w keys pressed');
+    }
+    if(keyPressed == '84'){
+        updateBlockImg('thor_left_hand.png');
+        updateBlockImg('thor_right_hand.png');
+        updateBlockImg('thor_face.png');
+        console.log('c + w keys pressed');
+    }
+}
